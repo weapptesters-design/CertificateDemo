c214a7861a24c6e9aab202ca84cb147f00dc5ddc
weapptesters-design <weapptesters@gmail.com>
2026-06-03 22:09:44 +0530

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,4 +1,4 @@
 window.APP_ASSETS = {
-  logo: "https://example.com/path/to/your/logo.png",
+  logo: "https://i.ibb.co/WN9Ss3Jv/IMG-20260601-161037-448.jpg",
   seal: "https://example.com/path/to/your/seal.png"
 };
