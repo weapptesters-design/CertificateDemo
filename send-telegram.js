09499a91f30444973f9b19a5f023fde452ebcfa7
weapptesters-design <weapptesters@gmail.com>
2026-06-04 03:01:02 +0530

    Update send-telegram.js

--- send-telegram.js
+++ send-telegram.js
@@ -5,17 +5,31 @@ const FormData = require("form-data");
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
-async function sendTelegram(filePath, appName) {
+async function send(filePath, appName) {
+
+  console.log("📤 Sending file:", filePath);
+
+  if (!fs.existsSync(filePath)) {
+    console.log("❌ FILE NOT FOUND:", filePath);
+    return;
+  }
 
   const form = new FormData();
   form.append("chat_id", CHAT_ID);
+  form.append("caption", `📄 ${appName}`);
   form.append("document", fs.createReadStream(filePath));
-  form.append("caption", `📄 Certificate Generated\n🧾 App: ${appName}`);
 
-  await fetch(`https://api.telegram.org/bot${TOKEN}/sendDocument`, {
-    method: "POST",
-    body: form
-  });
+  const res = await fetch(
+    `https://api.telegram.org/bot${TOKEN}/sendDocument`,
+    {
+      method: "POST",
+      body: form
+    }
+  );
+
+  const data = await res.json();
+
+  console.log("📡 Telegram response:", JSON.stringify(data, null, 2));
 }
 
-module.exports = sendTelegram;
+module.exports = send;
